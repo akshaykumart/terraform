db64ec5285ec4d2e4392bce4b6aa5fbf0458a167
Akshay Kumar T <106388941+akshaykumart@users.noreply.github.com>
2023-04-11 17:32:09 +0530

    Update security-group-rules.tf

--- security-group-rules.tf
+++ security-group-rules.tf
@@ -4,6 +4,26 @@ resource "aws_security_group_rule" "ingress_ssh" {
   to_port           = 22
   protocol          = "tcp"
   cidr_blocks       = ["0.0.0.0/0"]
-  ipv6_cidr_blocks  = ["0.0.0.0/0"]
-  security_group_id = aws_security_group.tcp
+  ipv6_cidr_blocks  = ["::/0"]
+  security_group_id = aws_security_group.allow_tcp.id
+}
+
+resource "aws_security_group_rule" "ingress_http" {
+  type              = "ingress"
+  from_port         = 80
+  to_port           = 80
+  protocol          = "tcp"
+  cidr_blocks       = ["0.0.0.0/0"]
+  ipv6_cidr_blocks  = ["::/0"]
+  security_group_id = aws_security_group.allow_tcp.id
+}
+
+resource "aws_security_group_rule" "egress" {
+  type              = "egress"
+  from_port         = 0
+  to_port           = 0
+  protocol          = "-1"
+  cidr_blocks       = ["0.0.0.0/0"]
+  ipv6_cidr_blocks  = ["::/0"]
+  security_group_id = aws_security_group.allow_tcp.id
 }
