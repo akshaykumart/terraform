c7f0718b1f474e665fe1c20e59617821cf5c2172
Akshay Kumar T <106388941+akshaykumart@users.noreply.github.com>
2023-04-11 17:26:51 +0530

    Create security-group-rules.tf

---
+++ security-group-rules.tf
@@ -0,0 +1,9 @@
+resource "aws_security_group_rule" "ingress_ssh" {
+  type              = "ingress"
+  from_port         = 22
+  to_port           = 22
+  protocol          = "tcp"
+  cidr_blocks       = ["0.0.0.0/0"]
+  ipv6_cidr_blocks  = ["0.0.0.0/0"]
+  security_group_id = aws_security_group.tcp
+}
