5c2468608e4fe4664cdb3e9fbc59f38caec8a16d
Akshay Kumar T <106388941+akshaykumart@users.noreply.github.com>
2023-04-11 15:00:37 +0530

    Create cloudfront_distribution.tf

---
+++ cloudfront_distribution.tf
@@ -0,0 +1,45 @@
+resource "aws_cloudfront_origin_access_control" "my_oac" {
+  name                              = "my_oac"
+  description                       = "my_oac Policy"
+  origin_access_control_origin_type = "s3"
+  signing_behavior                  = "always"
+  signing_protocol                  = "sigv4"
+}
+
+resource "aws_cloudfront_distribution" "s3_distribution" {
+  origin {
+    domain_name = aws_s3_bucket.bucket.bucket_regional_domain_name
+    origin_access_control_id = aws_cloudfront_origin_access_control.my_oac.id
+    origin_id   = local.s3_origin_id
+  }
+  enabled             = true
+  is_ipv6_enabled     = true
+  default_root_object = "home.html"
+
+  default_cache_behavior {
+    allowed_methods  = ["DELETE", "GET", "HEAD", "OPTIONS", "PATCH", "POST", "PUT"]
+    cached_methods   = ["GET", "HEAD"]
+    target_origin_id = local.s3_origin_id
+
+    forwarded_values {
+      query_string = false
+
+      cookies {
+        forward = "none"
+      }
+    }
+
+    viewer_protocol_policy = "allow-all"
+    min_ttl                = 0
+    default_ttl            = 3600
+    max_ttl                = 86400
+  }
+  restrictions {
+    geo_restriction {
+      restriction_type = "none"
+    }
+  }
+  viewer_certificate {
+    cloudfront_default_certificate = true
+  }
+}
