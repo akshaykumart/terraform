f9f0015a3621fe4b11ee3615d825dcde63b24eef
Akshay Kumar T <106388941+akshaykumart@users.noreply.github.com>
2023-04-12 12:25:44 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -9,3 +9,25 @@ variable "aws-ami-id" {
   description = "The AWS ami id to create ec2 instance"
   default = "ami-007855ac798b5175e"
 }
+
+variable "inst_type" {
+    description = "Multiple instance types"
+    type = list(string)
+    default = [ "t3.micro", "t3.small", "t3.medium" ]
+}
+
+variable "inst_count" {
+    description = "No of EC2 instances to be launched"
+    type = number
+    default = 1
+}
+
+variable "ec2_instance_tags" {
+   description = "Default tags to be used"
+   type =  map(string)
+   default = {
+       "Name" = "webapp"
+       "Environment" = "Dev"
+       "Type" = "Application"
+   }
+}
